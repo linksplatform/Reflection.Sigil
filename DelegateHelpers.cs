d616db3e33e9e082819149d21b7c01f44865e5b2
Константин Дьяченко <konard@me.com>
2019-07-18 19:34:42 +0300

    Initial code commit, forked from: https://github.com/Konard/LinksPlatform

---
+++ DelegateHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Sigil;
+
+namespace Platform.Reflection.Sigil
+{
+    public static class DelegateHelpers
+    {
+        public static void Compile<TDelegate>(out TDelegate @delegate, Action<Emit<TDelegate>> emitCode)
+        {
+            @delegate = default;
+            try
+            {
+                var emiter = Emit<TDelegate>.NewDynamicMethod();
+                emitCode(emiter);
+                @delegate = emiter.CreateDelegate();
+            }
+            finally
+            {
+                if (EqualityComparer<TDelegate>.Default.Equals(@delegate, default))
+                {
+                    var factory = new NotSupportedExceptionDelegateFactory<TDelegate>();
+                    @delegate = factory.Create();
+                }
+            }
+        }
+    }
+}
---
+++ NotSupportedExceptionDelegateFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sigil;
+using Platform.Interfaces;
+
+namespace Platform.Reflection.Sigil
+{
+    public class NotSupportedExceptionDelegateFactory<TDelegate> : IFactory<TDelegate>
+    {
+        public TDelegate Create()
+        {
+            var emiter = Emit<TDelegate>.NewDynamicMethod();
+            emiter.NewObject<NotSupportedException>();
+            emiter.Throw();
+            return emiter.CreateDelegate();
+        }
+    }
+}
